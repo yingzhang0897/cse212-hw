1adf30b66007c305a021efd9f02e180d2326656b
“yingzhang0897” <yinganticpc@gmail.com>
2024-04-23 21:31:33 -0700

    divisors finished

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("\n======================\nDivisors\n======================");
 Divisors.Run();
 
-Console.WriteLine("\n======================\nArray Selector\n======================");
-ArraySelector.Run();
+// Console.WriteLine("\n======================\nArray Selector\n======================");
+// ArraySelector.Run();
 
 // Uncomment when you are ready to run this
 // Console.WriteLine("\n======================\nAlgorithms\n======================");
