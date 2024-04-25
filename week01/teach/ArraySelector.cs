de49d57309e64f93c5f8fdb83bed7bf8c4b28aff
“yingzhang0897” <yinganticpc@gmail.com>
2024-04-25 11:47:01 -0700

    arraySelector done

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,13 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return new int[0];
+        var result = new int[select.Length];
+        var l1Idx = 0;
+        var l2Idx = 0;
+        for (int i = 0; i < select.Length; i++) 
+        {
+           result[i] = select[i] == 1 ? list1[l1Idx++]: list2[l2Idx++];
+        }
+        return result;
     }
 }
